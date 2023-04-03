1e3f9b097c5415f698e927693561c1abf6a7c45f
isabelsivira <117159819+isabelsivira@users.noreply.github.com>
2023-04-03 21:29:04 +0200

    done and eliminate buttons + css

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 //DOM selectors
-
+const todoList = document.querySelector(".todo-tasks")
 const todoInput = document.querySelector(".todo-input");
 const todoAddButton = document.querySelector(".todo-add-button");
-const todoList = document.querySelector(".todo-tasks")
 
 //Event listeners
-
-todoAddButton.addEventListener('click', addTodo);
+todoAddButton.addEventListener("click", addTodo);
 
 
 //Functions
-
 function addTodo(event) {
 
+   event.preventDefault();
     //creates a container for tasks
-    const toDoElement = document.createElement('div');
-    toDoElement.classList.add('toDoTask');
+    const toDoElement = document.createElement("div");
+    toDoElement.classList.add("toDoTask");
     
     //creates the list for tasks
-    const newTaskElement = document.createElement('li');
+    const newTaskElement = document.createElement("li");
+    newTaskElement.innerText = "hello";
+    newTaskElement.classList.add("new-task");
+    toDoElement.appendChild(newTaskElement);
 
+    const completedButton = document.createElement("button");
+    completedButton.innerText = "✅";
+    completedButton.classList.add("completed-button");
+    toDoElement.appendChild(completedButton);
+
+    const eliminateButton = document.createElement("button");
+    eliminateButton.innerText = "❌";
+    eliminateButton.classList.add("eliminate-button");
+    toDoElement.appendChild(eliminateButton);
+
+    //Add to the <ul> element
+    todoList.appendChild(toDoElement);
 }
+
+addTodo();
