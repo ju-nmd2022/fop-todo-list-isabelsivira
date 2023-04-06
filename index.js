023987406f5dc0e72dcd022df8d2308a7b6f38cd
isabelsivira <117159819+isabelsivira@users.noreply.github.com>
2023-04-06 14:12:54 +0200

    local storage working + other changes

--- index.js
+++ index.js
@@ -5,17 +5,16 @@ const todoAddButton = document.querySelector(".todo-add-button");
 
 //Event listeners
 todoAddButton.addEventListener("click", addTask);
-todoList.addEventListener("click", deleteTask);
+todoList.addEventListener("click", deleteAndComplete);
 
-
-//Functions
+//adds tasks to the list
 function addTask(event) {
-
-   event.preventDefault();
+event.preventDefault();
     //creates a container for tasks
     const toDoElement = document.createElement("div");
     toDoElement.classList.add("toDoTask");
-    
+    saveToLocalStorage(todoInput.value);
+
     //creates the list for tasks
     const newTaskElement = document.createElement("li");
     newTaskElement.innerText = todoInput.value;
@@ -25,10 +24,9 @@ function addTask(event) {
 
     //Done button
     const completedButton = document.createElement("button");
-    completedButton.innerText = "✅ ";
+    completedButton.innerText = "✅";
     completedButton.classList.add("completed-button");
     toDoElement.appendChild(completedButton);
-
     //Eliminate button
     const eliminateButton = document.createElement("button");
     eliminateButton.innerText = "❌";
@@ -39,10 +37,29 @@ function addTask(event) {
     todoList.appendChild(toDoElement);
 }
 
-function deleteTask(event) {
+//Deletes and completes the tasks
+function deleteAndComplete(event) {
     const item = event.target;
     if (item.classList[0] === "eliminate-button") {
         const task = item.parentElement;
         task.remove();
     }
+    if (item.classList[0] === "completed-button") {
+        const task = item.parentElement;
+        task.classList.toggle("done");
+    }
+}
+
+// The local storage part was adapted from this video https://www.youtube.com/watch?v=Ttf3CEsEwMQ&t=3673s&ab_channel=developedbyed and ChatGPT
+//Local storage
+function saveToLocalStorage(toDoTask) {
+    let tasks;
+    const storedTasks = localStorage.getItem("tasks");
+    if (storedTasks === null || !Array.isArray(JSON.parse(storedTasks))) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem("tasks"));
+    }
+    tasks.push(toDoTask);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
